632a24cbbc072304a4e1fab6dad358464543d81b
kgtkr <kgtkr.jp@gmail.com>
2017-10-12 23:51:05 +0900

    not-foundコンポーネント

---
+++ src/pages/not-found.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { Paper } from "material-ui";
+
+export interface NotFoundProps {
+
+}
+
+export function NotFound(_props: NotFoundProps) {
+  return <Paper>ページが見つかりません</Paper>;
+}
