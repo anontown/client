73c268e65ef5c5cf2f1dd8b822af7cb926f0daae
kgtkr <kgtkr.jp@gmail.com>
2017-10-14 10:38:03 +0900

    微修正

--- src/pages/not-found.tsx
+++ src/pages/not-found.tsx
@@ -1,10 +1,13 @@
 import * as React from "react";
 import { Paper } from "material-ui";
+import { Page } from "../components";
 
 export interface NotFoundProps {
 
 }
 
 export function NotFound(_props: NotFoundProps) {
-  return <Paper>ページが見つかりません</Paper>;
+  return <Page column={1}>
+    <Paper>ページが見つかりません</Paper>
+  </Page>;
 }
