b60e9c8a9f0c877e6ea924e083d06965aca3fa10
kgtkr <kgtkr.jp@gmail.com>
2017-10-14 11:35:57 +0900

    tags-inputコンポーネント作成

---
+++ src/components/tags-input.tsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import { apiClient } from "../utils";
+import { Snack } from "./snack";
+import { WithContext as TagInput } from 'react-tag-input';
+
+export interface TagsInputProps {
+  value: string[],
+  onChange?: (value: string[]) => void
+}
+
+
+export interface TagsInputState {
+  acTags: { name: string, count: number }[],
+  snackMsg: null | string
+}
+
+export class TagsInput extends React.Component<TagsInputProps, TagsInputState> {
+  constructor(props: TagsInputProps) {
+    super(props);
+    this.state = {
+      acTags: [],
+      snackMsg: null
+    }
+
+    apiClient.findTopicTags({ limit: 100 })
+      .subscribe(tags => {
+        this.setState({ acTags: tags });
+      }, () => {
+        this.setState({ snackMsg: "タグ候補取得に失敗しました" })
+      });
+  }
+
+  render() {
+    return [
+      <Snack
+        msg={this.state.snackMsg}
+        onHide={() => this.setState({ snackMsg: null })} />,
+      <TagInput placeholder="タグ"
+        tags={this.props.value.map((v, i) => ({ text: v, id: i }))}
+        suggestions={this.state.acTags.map(x => x.name)}
+        handleAddition={tag => {
+          if (this.props.onChange) {
+            this.props.onChange([...this.props.value, tag])
+          }
+        }}
+        handleDelete={i => {
+          if (this.props.onChange) {
+            const tags = [...this.props.value];
+            tags.splice(i, 1);
+            this.props.onChange(tags)
+          }
+        }} />
+    ];
+  }
+}
