9d6f32d3f3dc571b62346456a15a711954ebeda2
kgtkr <kgtkr.jp@gmail.com>
2017-11-02 21:30:40 +0900

    account-setting-page作成

---
+++ src/pages/account-setting.tsx
@@ -0,0 +1,97 @@
+import * as React from 'react';
+import { UserData } from "../models";
+import { ObjectOmit } from "typelevel-ts";
+import { connect } from "react-redux";
+import { Store } from "../reducers";
+import { updateUserData } from "../actions";
+import { RouteComponentProps } from "react-router-dom";
+import { apiClient } from "../utils";
+import { Snack, Errors } from "../components";
+import {
+  Paper,
+  TextField,
+  RaisedButton
+} from "material-ui";
+import { AtError } from "@anontown/api-client";
+
+type _AccountSettingPageProps = RouteComponentProps<{}> & {
+  user: UserData | null
+  updateUser: (user: UserData | null) => void;
+};
+
+export type AccountSettingPageProps = ObjectOmit<_AccountSettingPageProps, 'user' | 'updateUser'>;
+
+interface AccountSettingPageState {
+  newPass: string,
+  oldPass: string,
+  sn: string,
+  errors: string[],
+  snackMsg: string | null
+}
+
+class _AccountSettingPage extends React.Component<_AccountSettingPageProps, AccountSettingPageState> {
+  constructor(props: _AccountSettingPageProps) {
+    super(props);
+    this.state = {
+      snackMsg: null,
+      newPass: '',
+      oldPass: '',
+      sn: '',
+      errors: []
+    };
+
+    if (this.props.user !== null) {
+      apiClient
+        .findUserSN({ id: this.props.user.token.user })
+        .subscribe(sn => {
+          this.setState({ sn });
+        }, () => {
+          this.setState({ snackMsg: "ユーザー情報取得に失敗しました。" })
+        });
+    }
+  }
+
+  onSubmit() {
+    if (this.props.user === null) {
+      return;
+    }
+    const user = this.props.user;
+    apiClient.updateUser({ id: user.token.user, pass: this.state.oldPass }, {
+      pass: this.state.newPass,
+      sn: this.state.sn
+    })
+      .mergeMap(() => apiClient.createTokenMaster({ id: user.token.user, pass: this.state.newPass }))
+      .subscribe(token => {
+        this.props.updateUser({ ...user, token });
+        this.setState({ errors: [] });
+      }, error => {
+        if (error instanceof AtError) {
+          this.setState({ errors: error.errors.map(e => e.message) });
+        } else {
+          this.setState({ errors: ['エラーが発生しました'] });
+        }
+      });
+  }
+
+  render() {
+    return <Paper>
+      <Snack
+        msg={this.state.snackMsg}
+        onHide={() => this.setState({ snackMsg: null })} />
+      <form onSubmit={() => this.onSubmit()}>
+        <Errors errors={this.state.errors} />
+        <TextField floatingLabelText="ID" value={this.state.sn} onChange={(_e, v) => this.setState({ sn: v })} />
+        <TextField floatingLabelText="新しいパスワード" value={this.state.newPass} onChange={(_e, v) => this.setState({ newPass: v })} />
+        <TextField floatingLabelText="現在のパスワード" value={this.state.oldPass} onChange={(_e, v) => this.setState({ oldPass: v })} />
+        <RaisedButton type="submit" label="OK" />
+      </form>
+    </Paper >;
+  }
+}
+
+export const AccountSetting = connect(
+  (state: Store) => ({ user: state.user }),
+  dispatch => ({
+    updateUser: (user: UserData | null) => { dispatch(updateUserData(user)) }
+  })
+)(_AccountSettingPage);
