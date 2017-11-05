10ebdb91274b9ef1e9ff8ea5052efc717c9d855f
kgtkr <kgtkr.jp@gmail.com>
2017-11-05 19:23:20 +0900

    query-string採用

--- src/components/tags-link.tsx
+++ src/components/tags-link.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import { Link } from "react-router-dom";
+import * as qs from "query-string";
 
 export interface TagsLinkProps {
   tags: string[]
 }
 
 export function TagsLink(props: TagsLinkProps) {
-  return <Link to={{ pathname: "/topic/search", search: new URLSearchParams({ tags: props.tags.join(' ') }).toString() }}>
+  return <Link to={{ pathname: "/topic/search", search: qs.stringify({ tags: props.tags.join(' ') }) }}>
     {props.tags.length !== 0 ? props.tags.join(',') : '(なし)'}
   </Link>
 }
