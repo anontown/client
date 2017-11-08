c9c8786205cc77fcf4a6bf608561341725b8facb
kgtkr <kgtkr.jp@gmail.com>
2017-11-08 18:24:09 +0900

    バグ修正

--- src/utils/list.ts
+++ src/utils/list.ts
@@ -3,8 +3,8 @@ import * as Im from 'immutable';
 export function update<T extends { id: string }>(list: Im.List<T>, item: T) {
   const index = list.findIndex(x => x.id === item.id);
   if (index !== -1) {
-    list.set(index, item);
+    return list.set(index, item);
   } else {
-    list;
+    return list;
   }
 }
