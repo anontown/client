8580b362518a562d7bd8bdd2f86ccee7d999da70
kgtkr <kgtkr.jp@gmail.com>
2017-11-08 18:14:55 +0900

    アイテムが見つからない場合の対応

--- src/utils/list.ts
+++ src/utils/list.ts
@@ -1,5 +1,10 @@
 import * as Im from 'immutable';
 
 export function update<T extends { id: string }>(list: Im.List<T>, item: T) {
-  return list.set(list.findIndex(x => x.id === item.id), item);
+  const index = list.findIndex(x => x.id === item.id);
+  if (index !== -1) {
+    list.set(index, item);
+  } else {
+    list;
+  }
 }
