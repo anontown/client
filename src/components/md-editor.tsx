72df830ade0bd093f81a742a41595908659124b7
kgtkr <kgtkr.jp@gmail.com>
2017-11-20 23:35:34 +0900

    mat ui v1

--- src/components/md-editor.tsx
+++ src/components/md-editor.tsx
@@ -2,9 +2,10 @@ import {
   Dialog,
   IconButton,
   TextField,
-  Toggle,
+  Switch,
+  FormControlLabel
 } from "material-ui";
-import { ContentCreate, ImageAddAPhoto } from "material-ui/svg-icons";
+import * as icons from "material-ui-icons";
 import * as React from "react";
 import { Observable } from "rxjs";
 import { imgur } from "../utils";
@@ -45,13 +46,12 @@ export class MdEditor extends React.Component<MdEditorProps, MdEditorState> {
         <Dialog
           title="お絵かき"
           open={this.state.slowOekaki}
-          autoScrollBodyContent={true}
           onRequestClose={() => this.setState({ slowOekaki: false })}>
           <Errors errors={this.state.oekakiErrors} />
-          <Oekaki size={{ x: 320, y: 240 }} onSubmit={ svg => {
+          <Oekaki size={{ x: 320, y: 240 }} onSubmit={svg => {
             const data = new Blob([svg], { type: "image/svg+xml" });
             imgur.upload(data)
-              .subscribe( url => {
+              .subscribe(url => {
                 this.setState({ slowOekaki: false, oekakiErrors: undefined });
                 if (this.props.onChange) {
                   this.props.onChange(this.props.value + `![](${url})`);
@@ -64,23 +64,22 @@ export class MdEditor extends React.Component<MdEditorProps, MdEditorState> {
         <Dialog
           title="画像アップロード"
           open={this.state.slowImage}
-          autoScrollBodyContent={true}
           onRequestClose={() => this.setState({ slowImage: false })}>
           <Errors errors={this.state.imageErrors} />
-          <IconButton type="file" onChange={ e => {
+          <IconButton type="file" onChange={(e: any) => {
             const target = e.target as HTMLInputElement;
             const files = target.files;
             if (files !== null) {
               Observable.of(...Array.from(files))
-                .map( file => {
+                .map(file => {
                   const formData = new FormData();
                   formData.append("image", file);
                   return formData;
                 })
-                .mergeMap( form => imgur.upload(form))
-                .map( url => `![](${url})`)
+                .mergeMap(form => imgur.upload(form))
+                .map(url => `![](${url})`)
                 .reduce((tags, tag) => tags + tag + "\n", "")
-                .subscribe( tags => {
+                .subscribe(tags => {
                   this.setState({ slowImage: false, oekakiErrors: undefined });
                   if (this.props.onChange) {
                     this.props.onChange(this.props.value + tags);
@@ -90,28 +89,27 @@ export class MdEditor extends React.Component<MdEditorProps, MdEditorState> {
                 });
             }
           }}>
-            <ImageAddAPhoto />
+            <icons.AddAPhoto />
           </IconButton>
         </Dialog>
         <div>
           <IconButton onClick={() => this.setState({ slowImage: true })}>
-            <ImageAddAPhoto />
+            <icons.AddAPhoto />
           </IconButton>
           <IconButton onClick={() => this.setState({ slowOekaki: true })}>
-            <ContentCreate />
+            <icons.Create />
           </IconButton>
         </div>
-        <Toggle
-          label="Preview"
-          defaultToggled={this.state.preview}
-          onToggle={(_e, v) => this.setState({ preview: v })} />
-        <TextField multiLine={true}
+        <FormControlLabel label="Preview" control={<Switch
+          checked={this.state.preview}
+          onChange={(_e, v) => this.setState({ preview: v })} />} />
+        <TextField multiline={true}
           rows={this.props.minRows || this.defaltMinRows}
           rowsMax={this.props.maxRows}
           value={this.props.value}
-          onChange={(_, v) => {
+          onChange={e => {
             if (this.props.onChange) {
-              this.props.onChange(v);
+              this.props.onChange(e.target.value);
             }
           }} />
         <Md body={this.props.value} />
