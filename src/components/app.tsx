74f1e27a98768d3a447939c0894edb8d82170c8e
kgtkr <kgtkr.jp@gmail.com>
2017-11-21 21:27:47 +0900

    バグ修正

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -54,69 +54,67 @@ export const App = connect((state: Store) => ({ user: state.user }), dispatch =>
   render() {
     return (
       <MuiThemeProvider muiTheme={muiTheme}>
-        <Switch>
-          <div>
-            <Toolbar className={style.toolbar}>
-              <ToolbarGroup firstChild={true}>
-                <ToolbarTitle text="Anontown" />
-              </ToolbarGroup>
-              <ToolbarGroup>
-                <IconButton containerElement={<Link to="/" />}>
-                  <icons.ActionHome />
+        <div>
+          <Toolbar className={style.toolbar}>
+            <ToolbarGroup firstChild={true}>
+              <ToolbarTitle text="Anontown" />
+            </ToolbarGroup>
+            <ToolbarGroup>
+              <IconButton containerElement={<Link to="/" />}>
+                <icons.ActionHome />
+              </IconButton>
+              <IconButton containerElement={<Link to="/topic/search" />}>
+                <icons.ActionSearch />
+              </IconButton>
+              {this.props.user !== null
+                ? <IconButton containerElement={<Link to="/notifications" />}>
+                  <icons.NotificationAdb />
                 </IconButton>
-                <IconButton containerElement={<Link to="/topic/search" />}>
-                  <icons.ActionSearch />
-                </IconButton>
-                {this.props.user !== null
-                  ? <IconButton containerElement={<Link to="/notifications" />}>
-                    <icons.NotificationAdb />
+                : null}
+              <IconMenu
+                iconButtonElement={
+                  <IconButton touch={true}>
+                    <icons.ActionVerifiedUser />
                   </IconButton>
-                  : null}
-                <IconMenu
-                  iconButtonElement={
-                    <IconButton touch={true}>
-                      <icons.ActionVerifiedUser />
-                    </IconButton>
-                  }>
-                  {this.props.user !== null
-                    ? [
-                      <MenuItem primaryText="プロフ管理" containerElement={<Link to="/profiles" />} />,
-                      <MenuItem primaryText="お知らせ" containerElement={<Link to="/messages" />} />,
-                      <MenuItem primaryText="設定" containerElement={<Link to="/settings/account" />} />,
-                      <MenuItem primaryText="プロフ管理" containerElement={<Link to="/profiles" />} />,
-                      <MenuItem primaryText="ログアウト" onClick={() => this.logout()} />,
-                    ]
-                    : <MenuItem primaryText="ログイン/登録" containerElement={<Link to="/in" />} />}
+                }>
+                {this.props.user !== null
+                  ? [
+                    <MenuItem primaryText="プロフ管理" containerElement={<Link to="/profiles" />} />,
+                    <MenuItem primaryText="お知らせ" containerElement={<Link to="/messages" />} />,
+                    <MenuItem primaryText="設定" containerElement={<Link to="/settings/account" />} />,
+                    <MenuItem primaryText="プロフ管理" containerElement={<Link to="/profiles" />} />,
+                    <MenuItem primaryText="ログアウト" onClick={() => this.logout()} />,
+                  ]
+                  : <MenuItem primaryText="ログイン/登録" containerElement={<Link to="/in" />} />}
 
-                </IconMenu>
-                <IconButton containerElement={<a
-                  href="https://document.anontown.com/"
-                  target="_blank" />}>
-                  <icons.ActionBook />
-                </IconButton>
-                <IconButton onClick={() => this.changeTheme()}>
-                  <icons.ActionInvertColors />
-                </IconButton>
-              </ToolbarGroup>
-            </Toolbar>
-            <div className={style.main}>
-              <Switch>
-                <Route exact path="/" component={pages.HomePage} />
-                <Route path="/res/:id" component={pages.ResPage} />
-                <Route path="/topic/search" component={pages.TopicSearchPage} />
-                <Route path="/topic/create" component={pages.TopicCreatePage} />
-                <Route path="/topic/:id" component={pages.TopicPage} />
-                <Route path="/profiles" component={pages.ProfilesPage} />
-                <Route path="/notifications" component={pages.NotificationsPage} />
-                <Route path="/messages" component={pages.MessagesPage} />
-                <Route path="/in" component={pages.InPage} />
-                <Route path="/auth" component={pages.AuthPage} />
-                <Route path="/settings" component={pages.SettingsPage} />
-                <Route component={pages.NotFoundPage} />
-              </Switch>
-            </div>
+              </IconMenu>
+              <IconButton containerElement={<a
+                href="https://document.anontown.com/"
+                target="_blank" />}>
+                <icons.ActionBook />
+              </IconButton>
+              <IconButton onClick={() => this.changeTheme()}>
+                <icons.ActionInvertColors />
+              </IconButton>
+            </ToolbarGroup>
+          </Toolbar>
+          <div className={style.main}>
+            <Switch>
+              <Route exact path="/" component={pages.HomePage} />
+              <Route path="/res/:id" component={pages.ResPage} />
+              <Route path="/topic/search" component={pages.TopicSearchPage} />
+              <Route path="/topic/create" component={pages.TopicCreatePage} />
+              <Route path="/topic/:id" component={pages.TopicPage} />
+              <Route path="/profiles" component={pages.ProfilesPage} />
+              <Route path="/notifications" component={pages.NotificationsPage} />
+              <Route path="/messages" component={pages.MessagesPage} />
+              <Route path="/in" component={pages.InPage} />
+              <Route path="/auth" component={pages.AuthPage} />
+              <Route path="/settings" component={pages.SettingsPage} />
+              <Route component={pages.NotFoundPage} />
+            </Switch>
           </div>
-        </Switch>
+        </div>
       </MuiThemeProvider>
     );
   }
