5f8558589dba426ad5e62790077187d0a6747194
kgtkr <kgtkr.jp@gmail.com>
2017-11-22 21:20:33 +0900

    綺麗に書きなおした

--- src/components/md.tsx
+++ src/components/md.tsx
@@ -14,180 +14,188 @@ export interface MdProps {
   body: string;
 }
 
-interface MdState {
-  youtube: string | null;
+export function Md(props: MdProps) {
+  const node = mdParser.parse(props.body);
+  return <div>{node.children.map(c => <MdNode node={c} />)}</div>;
 }
 
-export class Md extends React.Component<MdProps, MdState> {
-  constructor(props: MdProps) {
+interface MdYouTubeProps {
+  title?: string,
+  videoID: string
+}
+
+class MdYouTube extends React.Component<MdYouTubeProps, { slow: boolean }>{
+  constructor(props: MdYouTubeProps) {
     super(props);
     this.state = {
-      youtube: null
-    }
+      slow: false
+    };
   }
 
   render() {
-    const node = mdParser.parse(this.props.body);
-    return (
-      <div>
-        <Dialog
-          title="YouTube"
-          open={this.state.youtube !== null}
-          autoScrollBodyContent={true}
-          onRequestClose={() => this.setState({ youtube: null })}>
-          {this.state.youtube !== null
-            ? <YouTube videoId={this.state.youtube} />
-            : null}
-        </Dialog>
-        {node.children.map(x => this.renderNode(x))}
-      </div>
-    );
+    return [
+      <img src={`https://i.ytimg.com/vi/${this.props.videoID}/maxresdefault.jpg`}
+        title={this.props.title || undefined}
+        onClick={() => this.setState({ slow: true })} />,
+      <Dialog
+        title="YouTube"
+        open={this.state.slow}
+        autoScrollBodyContent={true}
+        onRequestClose={() => this.setState({ slow: false })}>
+        {this.state.slow
+          ? <YouTube videoId={this.props.videoID} />
+          : null}
+      </Dialog>
+    ]
+  }
+}
+
+function urlEnum(url: string): URLType {
+  const reg = url.match(/(youtube\.com\/watch\?v=|youtu\.be\/)([a-z0-9_]+)/i);
+  if (reg) {
+    return { type: "youtube", videoID: reg[2] };
   }
 
-  renderTable(node: mdParser.Table): JSX.Element {
-    const head = node.children[0];
-
-    return (<table>
-      <thead>
-        <tr>
-          {head.type === "tableRow"
-            ? head.children.map((cell, index) =>
-              <th style={{ textAlign: node.align[index] }}>{cell.type === "tableCell"
-                ? cell.children.map(x => this.renderNode(x))
-                : null}</th>)
-            : null}
-        </tr>
-      </thead>
-      <tbody>
-        {node.children
-          .filter((_, i) => i !== 0)
-          .map(row => row.type === "tableRow"
-            ? <tr>
-              {row.children.map((cell, index) => cell.type === "tableCell"
-                ? <td style={{ textAlign: node.align[index] }}>{cell.children.map(x => this.renderNode(x))}</td>
-                : null)}
-            </tr>
-            : null)}
-      </tbody >
-    </table >);
+  if (url.indexOf("http://") !== 0 && url.indexOf("https://") !== 0) {
+    return {
+      type: "router",
+      path: url,
+    };
   }
 
-  renderHeading(node: mdParser.Heading): JSX.Element {
-    switch (node.depth) {
-      case 1:
-        return (<h1>
-          {node.children.map(c => this.renderNode(c))}
-        </h1>);
-      case 2:
-        return (<h2>
-          {node.children.map(c => this.renderNode(c))}
-        </h2>);
-      case 3:
-        return (<h3>
-          {node.children.map(c => this.renderNode(c))}
-        </h3>);
-      case 4:
-        return (<h4>
-          {node.children.map(c => this.renderNode(c))}
-        </h4>);
-      case 5:
-        return (<h5>
-          {node.children.map(c => this.renderNode(c))}
-        </h5>);
-      case 6:
-        return (<h6>
-          {node.children.map(c => this.renderNode(c))}
-        </h6>);
-    }
+  if (url.indexOf(Config.client.origin) === 0) {
+    return {
+      type: "router",
+      path: url.substring(Config.client.origin.length),
+    };
   }
 
-  renderLink(node: mdParser.Link): JSX.Element {
-    const link = this.urlEnum(node.url);
-    switch (link.type) {
-      case "normal":
-        return <a href={node.url}
-          target="_blank"
-          title={node.title || undefined}>{node.children.map(c => this.renderNode(c))}</a>;
-      case "image":
-        return <img
-          src={camo.getCamoUrl(node.url)}
-          title={node.title || undefined} />;
-      case "youtube":
-        return <img src={`http://i.ytimg.com/vi/${link.videoID}/maxresdefault.jpg`}
-          title={node.title || undefined}
-          onClick={() => this.setState({ youtube: link.videoID })} />;
-      case "router":
-        return <Link to={link.path}>{node.children.map(c => this.renderNode(c))}</Link>;
-    }
+  if (url.match(/\.(jpg|jpeg|png|gif|bmp|tif|tiff|svg)$/i)) {
+    return { type: "image", url };
   }
 
-  urlEnum(url: string): URLType {
-    const reg = url.match(/(youtube\.com\/watch\?v=|youtu\.be\/)([a-z0-9_]+)/i);
-    if (reg) {
-      return { type: "youtube", videoID: reg[2] };
-    }
+  return { type: "normal", url };
+}
 
-    if (url.indexOf("http://") !== 0 && url.indexOf("https://") !== 0) {
-      return {
-        type: "router",
-        path: url,
-      };
-    }
-
-    if (url.indexOf(Config.client.origin) === 0) {
-      return {
-        type: "router",
-        path: url.substring(Config.client.origin.length),
-      };
-    }
-
-    if (url.match(/\.(jpg|jpeg|png|gif|bmp|tif|tiff|svg)$/i)) {
-      return { type: "image", url };
-    }
-
-    return { type: "normal", url };
+function MdLink(props: { node: mdParser.Link }) {
+  const link = urlEnum(props.node.url);
+  switch (link.type) {
+    case "normal":
+      return <a href={props.node.url}
+        target="_blank"
+        title={props.node.title || undefined}>{props.node.children.map(c => <MdNode node={c} />)}</a>;
+    case "image":
+      return <img
+        src={camo.getCamoUrl(props.node.url)}
+        title={props.node.title || undefined} />;
+    case "youtube":
+      return <MdYouTube videoID={link.videoID} title={props.node.title || undefined} />;
+    case "router":
+      return <Link to={link.path}>{props.node.children.map(c => <MdNode node={c} />)}</Link>;
   }
+}
 
-  renderNode(node: mdParser.MdNode): JSX.Element | string | null {
-    switch (node.type) {
+function MdHeading(props: { node: mdParser.Heading }) {
+  switch (props.node.depth) {
+    case 1:
+      return <h1>
+        {props.node.children.map(c => <MdNode node={c} />)}
+      </h1>;
+    case 2:
+      return <h2>
+        {props.node.children.map(c => <MdNode node={c} />)}
+      </h2>;
+    case 3:
+      return <h3>
+        {props.node.children.map(c => <MdNode node={c} />)}
+      </h3>;
+    case 4:
+      return <h4>
+        {props.node.children.map(c => <MdNode node={c} />)}
+      </h4>;
+    case 5:
+      return <h5>
+        {props.node.children.map(c => <MdNode node={c} />)}
+      </h5>;
+    case 6:
+      return <h6>
+        {props.node.children.map(c => <MdNode node={c} />)}
+      </h6>;
+  }
+}
+
+function MdTable(props: { node: mdParser.Table }) {
+  const head = props.node.children[0];
+
+  return <table>
+    <thead>
+      <tr>
+        {head.type === "tableRow"
+          ? head.children.map((cell, index) =>
+            <th style={{ textAlign: props.node.align[index] }}>{cell.type === "tableCell"
+              ? cell.children.map(c => <MdNode node={c} />)
+              : null}</th>)
+          : null}
+      </tr>
+    </thead>
+    <tbody>
+      {props.node.children
+        .filter((_, i) => i !== 0)
+        .map(row => row.type === "tableRow"
+          ? <tr>
+            {row.children.map((cell, index) => cell.type === "tableCell"
+              ? <td style={{ textAlign: props.node.align[index] }}>{cell.children.map(c => <MdNode node={c} />)}</td>
+              : null)}
+          </tr>
+          : null)}
+    </tbody >
+  </table>;
+}
+
+class MdNode extends React.Component<{ node: mdParser.MdNode }, {}>{
+  render(): React.ReactNode {
+    switch (this.props.node.type) {
       case "paragraph":
-        return (<p>{node.children.map(c => this.renderNode(c))}</p>);
+        return <p>{this.props.node.children.map(c => <MdNode node={c} />)}</p>;
       case "blockquote":
-        return (<blockquote>{node.children.map(c => this.renderNode(c))}</blockquote>);
+        return <blockquote>{this.props.node.children.map(c => <MdNode node={c} />)}</blockquote>;
       case "heading":
-        return this.renderHeading(node);
+        return <MdHeading node={this.props.node} />;
       case "code":
-        return (<pre>
-          <code>{node.value}</code>
-        </pre>);
+        return <pre>
+          <code>{this.props.node.value}</code>
+        </pre>;
       case "inlineCode":
-        return (<code>{node.value}</code>);
+        return <code>{this.props.node.value}</code>;
       case "list":
-        if (node.ordered) {
-          return (<ol>{node.children.map(c => this.renderNode(c))}</ol>);
+        if (this.props.node.ordered) {
+          return <ol>{this.props.node.children.map(c => <MdNode node={c} />)}</ol>;
         } else {
-          return (<ul>{node.children.map(c => this.renderNode(c))}</ul>);
+          return <ul>{this.props.node.children.map(c => <MdNode node={c} />)}</ul>;
         }
       case "listItem":
-        return (<li>{node.children.map(c => this.renderNode(c))}</li>);
+        return <li>{this.props.node.children.map(c => <MdNode node={c} />)}</li>;
       case "table":
-        return this.renderTable(node);
+        return <MdTable node={this.props.node} />;
       case "thematicBreak":
         return <hr />;
       case "break":
         return <br />;
       case "emphasis":
-        return (<em>{node.children.map(c => this.renderNode(c))}</em>);
+        return <em>{this.props.node.children.map(c => <MdNode node={c} />)}</em>;
       case "strong":
-        return (<strong>{node.children.map(c => this.renderNode(c))}</strong>);
+        return <strong>{this.props.node.children.map(c => <MdNode node={c} />)}</strong>;
       case "delete":
-        return (<del>{node.children.map(c => this.renderNode(c))}</del>);
+        return <del>{this.props.node.children.map(c => <MdNode node={c} />)}</del>;
       case "link":
-        return this.renderLink(node);
+        return <MdLink node={this.props.node} />;
       case "image":
-        return (<img src={camo.getCamoUrl(node.url)} title={node.title || undefined} alt={node.alt || undefined} />);
+        return <img
+          src={camo.getCamoUrl(this.props.node.url)}
+          title={this.props.node.title || undefined}
+          alt={this.props.node.alt || undefined} />;
       case "text":
-        return node.value;
+        return this.props.node.value;
       default:
         return null;
     }
