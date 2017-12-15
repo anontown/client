68085f9e623fab61f39516bf8b974a0e0e2d5d00
kgtkr <kgtkr.jp@gmail.com>
2017-12-15 22:46:38 +0900

    バグ修正

--- src/pages/settings/settings.tsx
+++ src/pages/settings/settings.tsx
@@ -22,19 +22,17 @@ export const SettingsPage = withRouter(class extends React.Component<SettingsPag
   }
 
   render() {
-    return (
-      <Page
-        sidebar={<List>
-          <ListItem><Link to="/settings/account">アカウント設定</Link></ListItem>
-          <ListItem><Link to="/settings/apps">連携アプリ</Link></ListItem>
-          <ListItem><Link to="/settings/dev">開発者向け</Link></ListItem>
-        </List>}>
-        <Switch>
-          <Route path="/settings/account" component={AccountSettingPage} />
-          <Route path="/settings/apps" component={AppsSettingPage} />
-          <Route path="/settings/dev" component={DevSettingPage} />
-        </Switch>
-      </Page>
-    );
+    return <Page
+      sidebar={<List>
+        <ListItem containerElement={<Link to="/settings/account" />}>アカウント設定</ListItem>
+        <ListItem containerElement={<Link to="/settings/apps" />}>連携アプリ</ListItem>
+        <ListItem containerElement={<Link to="/settings/dev" />}>開発者向け</ListItem>
+      </List>}>
+      <Switch>
+        <Route path="/settings/account" component={AccountSettingPage} />
+        <Route path="/settings/apps" component={AppsSettingPage} />
+        <Route path="/settings/dev" component={DevSettingPage} />
+      </Switch>
+    </Page>;
   }
 });
