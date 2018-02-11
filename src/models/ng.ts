0441196b6b1f6d69cfbd0439fd44c64d301cc2f7
kgtkr <kgtkr.jp@gmail.com>
2018-02-11 18:08:00 +0900

    NG機能のモデル

---
+++ src/models/ng.ts
@@ -0,0 +1,63 @@
+export interface NG {
+  topic: string | null,
+  transparent: boolean,
+  date: string,
+  expirationDate: string,
+  chain: number | null,
+  body: NGBody
+}
+
+export type NGBody = NGBodyNot |
+  NGBodyAnd |
+  NGBodyOr |
+  NGBodyProfile |
+  NGBodyHash |
+  NGBodyBody |
+  NGBodyName |
+  NGBodyVote;
+
+export interface NGBodyNot {
+  type: "not",
+  body: NGBody
+}
+
+export interface NGBodyAnd {
+  type: "and",
+  body: NGBody[]
+}
+
+export interface NGBodyOr {
+  type: "or",
+  body: NGBody[]
+}
+
+export interface NGBodyProfile {
+  type: "profile",
+  id: string
+}
+
+export interface NGBodyHash {
+  type: "hash",
+  id: string
+}
+
+export interface NGBodyReg {
+  reg: string,
+  g: boolean,
+  i: boolean,
+  m: boolean,
+  y: boolean
+}
+
+export interface NGBodyBody extends NGBodyReg {
+  type: "body"
+}
+
+export interface NGBodyName extends NGBodyReg {
+  type: "name"
+}
+
+export interface NGBodyVote {
+  type: "vote",
+  value: number
+}
