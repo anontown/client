4abac5cbddee7b70be597f5c1b3b9520bf184850
kgtkr <kgtkr.jp@gmail.com>
2018-02-16 21:08:23 +0900

    追加と削除

--- src/components/ng.tsx
+++ src/components/ng.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import { list } from "../utils";
 import { UserStore, appInject } from "../stores";
 import { NGEditor } from "./ng-editor";
+import { IconButton, FontIcon } from "material-ui";
+import { ng } from "../models";
 
 interface NGProps {
   user: UserStore
@@ -20,15 +22,33 @@ export const NG = appInject(class extends React.Component<NGProps, NGState> {
     return user !== null
       ? <div>
         {user.storage.ng.map(ng =>
-          <NGEditor
-            key={ng.id}
-            ng={ng}
-            onUpdate={v => this.props.user.setData({
+          <div>
+            <IconButton onClick={() => this.props.user.setData({
               ...user, storage: {
                 ...user.storage,
-                ng: list.update(user.storage.ng, v)
+                ng: user.storage.ng.filter(x => x.id !== ng.id)
               }
-            })} />)}
+            })}>
+              <FontIcon className="material-icons">close</FontIcon>
+            </IconButton>
+            <NGEditor
+              key={ng.id}
+              ng={ng}
+              onUpdate={v => this.props.user.setData({
+                ...user, storage: {
+                  ...user.storage,
+                  ng: list.update(user.storage.ng, v)
+                }
+              })} />
+          </div>)}
+        <IconButton onClick={() => this.props.user.setData({
+          ...user, storage: {
+            ...user.storage,
+            ng: user.storage.ng.push(ng.createDefaultNG())
+          }
+        })}>
+          <FontIcon className="material-icons">note_add</FontIcon>
+        </IconButton>
       </div>
       : <div>ログインして下さい</div>;
   }
