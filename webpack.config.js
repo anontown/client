8590b21106fa699bdb5c10814bc3e1c0b1f5b35c
tkr <kgtkr.jp@gmail.com>
2018-05-27 17:18:44 +0900

    スタイルとか

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     resolve: {
         extensions: [".ts", ".tsx", ".js"],
         alias: {
-            style$: path.resolve(__dirname, 'src/style')
+            style: path.resolve(__dirname, 'src/style')
         }
     },
     plugins: [
