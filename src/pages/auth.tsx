e780c6b7cf287a34aee83b7fe2d8d797e5a49c40
kgtkr <kgtkr.jp@gmail.com>
2018-08-14 15:14:30 +0900

    途中まで

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -10,44 +10,58 @@ import {
 import { Snack } from "../components";
 import { Page } from "../components";
 import {
-  AuthStore,
   myInject,
   UserStore,
 } from "../stores";
+import { Query, Mutation } from "react-apollo";
+import { getClient, createToken, createTokenReq } from "./auth.gql";
+import { getClient as getClientResult, getClientVariables } from "./_gql/getClient";
+import { createToken as createTokenResult, createTokenVariables } from "./_gql/createToken";
+import { createTokenReq as createTokenReqResult } from "./_gql/createTokenReq";
 
 interface AuthPageProps extends RouteComponentProps<{}> {
   user: UserStore;
-  auth: AuthStore;
 }
 
 interface AuthPageState {
 }
 
-export const AuthPage = withRouter(myInject(["user", "auth"],
+export const AuthPage = withRouter(myInject(["user"],
   observer(class extends React.Component<AuthPageProps, AuthPageState> {
     constructor(props: AuthPageProps) {
       super(props);
-
-      const id: string | string[] | undefined = qs.parse(this.props.location.search).client;
-      if (typeof id === "string") {
-        this.props.auth.load(id);
-      }
     }
 
     render() {
+      const id: string | string[] | undefined = qs.parse(this.props.location.search).client;
+
+
       return <Page>
         <Helmet>
           <title>アプリ認証</title>
         </Helmet>
-        <Snack
-          msg={this.props.auth.msg}
-          onHide={() => this.props.auth.clearMsg()} />
-        {this.props.user.data !== null && this.props.auth.client !== null ? <div>
-          認証しますか？
-        <RaisedButton type="button" label="OK" onClick={() => this.props.auth.auth()} />
-        </div>
-          : this.props.user.data === null ? <div>ログインして下さい</div>
-            : null}
+        {this.props.user.data !== null ? typeof id === "string"
+          ? <Query<getClientResult, getClientVariables>
+            query={getClient}
+            variables={{ id: id }}>
+            {({ loading, error, data }) => {
+              if (loading) return "Loading...";
+              if (error || !data || data.clients.length === 0) return (<Snack msg="クライアント取得に失敗しました。" />);
+
+              return (<Mutation<createTokenResult, createTokenVariables>
+                mutation={createToken}
+                variables={{ client: data.clients[0].id }}>
+                {(create, { data, error, loading }) => {
+                  return <div>
+                    認証しますか？
+                  <RaisedButton type="button" label="OK" onClick={() => create()} />
+                  </div>;
+                }}
+              </Mutation>);
+            }}
+          </Query>
+          : <div>パラメーターが不正です</div>
+          : <div>ログインして下さい</div>}
       </Page>;
     }
   })));
