2dac8fbb9f82906a69f087e14049093881298610
kgtkr <kgtkr.jp@gmail.com>
2018-08-21 18:01:18 +0900

    topic-data

--- src/components/topic-data.tsx
+++ src/components/topic-data.tsx
@@ -1,58 +1,34 @@
-import * as api from "@anontown/api-types";
 import * as React from "react";
 import { Link } from "react-router-dom";
-import { apiClient, dateFormat } from "../utils";
+import { dateFormat } from "../utils";
 import { History } from "./history";
 import { Md } from "./md";
 import { Snack } from "./snack";
 import { TagsLink } from "./tags-link";
+import { topic } from "../gql/_gql/topic";
 
 export interface TopicDataProps {
-  topic: api.Topic;
+  topic: topic;
 }
 
 interface TopicDataState {
-  histories: api.History[] | null;
-  parent: api.Topic | null;
-  snackMsg: null | string;
 }
 
 export class TopicData extends React.Component<TopicDataProps, TopicDataState> {
   constructor(props: TopicDataProps) {
     super(props);
     this.state = {
-      histories: null,
-      parent: null,
-      snackMsg: null,
     };
-
-    (async () => {
-      try {
-        if (this.props.topic.type === "normal") {
-          const histories = await apiClient.findHistoryAll({ topic: this.props.topic.id });
-          this.setState({ histories });
-        } else if (this.props.topic.type === "fork") {
-          const parent = await apiClient.findTopicOne({ id: this.props.topic.parent });
-          this.setState({ parent });
-        }
-      } catch {
-        this.setState({ snackMsg: "履歴取得に失敗しました" });
-      }
-    })();
   }
   render() {
     return <div>
       <dl>
-        <Snack
-          msg={this.state.snackMsg}
-          onHide={() => this.setState({ snackMsg: null })} />
         <dt>作成</dt>
         <dd>{dateFormat.format(this.props.topic.date)}</dd>
         <dt>更新</dt>
         <dd>{dateFormat.format(this.props.topic.update)}</dd>
-        {this.props.topic.type !== "fork"
-          ?
-          <div>
+        {this.props.topic.__typename !== "TopicFork"
+          ? <>
             <dt>カテゴリ</dt>
             <dd>
               <TagsLink tags={this.props.topic.tags} />
@@ -61,26 +37,20 @@ export class TopicData extends React.Component<TopicDataProps, TopicDataState> {
             <dd>
               <Md text={this.props.topic.text} />
             </dd>
-          </div>
+          </>
           : null
         }
-        {this.state.parent !== null
-          ? <div>
+        {this.props.topic.__typename === "TopicFork"
+          ? <>
             <dt>派生元</dt>
             <dd>
-              <Link to={`/topic/${this.state.parent.id}`}>{this.state.parent.title}</Link>
+              <Link to={`/topic/${this.props.topic.parent.id}`}>{this.props.topic.parent.title}</Link>
             </dd>
-
-          </div>
+          </>
           : null}
-        {this.state.histories !== null
-          ? <div>
-            <dt>編集履歴</dt>
-            <dd>
-              {this.state.histories.map(h => <History key={h.id} history={h} />)}
-            </dd>
-          </div>
-          : null}
+        {
+          //TODO: 編集履歴を別ページにしてここからリンク
+        }
       </dl>
     </div>;
   }
