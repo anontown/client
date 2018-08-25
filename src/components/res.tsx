195e213d388aa933f1e356b29f00660335798196
kgtkr <kgtkr.jp@gmail.com>
2018-08-25 17:03:48 +0900

    Res

--- src/components/res.tsx
+++ src/components/res.tsx
@@ -1,4 +1,3 @@
-import * as api from "@anontown/api-types";
 import * as classNames from "classnames";
 import {
   FontIcon,
@@ -12,23 +11,26 @@ import * as React from "react";
 import { Link } from "react-router-dom";
 import { Omit } from "type-zoo";
 import * as uuid from "uuid";
-import { ResSeted } from "../models";
 import { ng } from "../models";
 import { myInject, UserStore } from "../stores";
 import {
-  apiClient,
   dateFormat,
-  resSetedCreate,
 } from "../utils";
 import { Md } from "./md";
 import { ResWrite } from "./res-write";
 import * as style from "./res.scss";
 import { Snack } from "./snack";
+import { res } from "../gql/_gql/res";
+import { voteRes, delRes } from "./res.gql";
+import { voteRes as voteResResult, voteResVariables } from "./_gql/voteRes";
+import { delRes as delResResult, delResVariables } from "./_gql/delRes";
+import { Mutation } from "react-apollo";
+import { VoteType } from "_gql/globalTypes";
 
 interface UnconnectedResProps {
-  res: ResSeted;
+  res: res;
   user: UserStore;
-  update?: (res: ResSeted) => void;
+  update?: (res: res) => void;
 }
 
 export type ResProps = Omit<UnconnectedResProps, "user">;
@@ -49,75 +51,6 @@ export const Res = myInject(["user"], observer(class extends React.Component<Unc
     };
   }
 
-  async vote(token: api.Token, vote: () => Promise<api.Res>) {
-    try {
-      const reses = await resSetedCreate.resSet(token, [await vote()]);
-      const res = reses[0];
-      if (this.props.update) {
-        this.props.update(res);
-      }
-    } catch {
-      this.setState({ snackMsg: "投票に失敗しました" });
-    }
-  }
-
-  async onUV() {
-    const user = this.props.user.data;
-    if (user === null) {
-      return;
-    }
-
-    switch (this.props.res.voteFlag) {
-      case "uv":
-        await this.vote(user.token, () => apiClient.cvRes(user.token, { id: this.props.res.id }));
-        break;
-      case "dv":
-        await this.vote(user.token, () => apiClient.cvRes(user.token, { id: this.props.res.id })
-          .then(() => apiClient.uvRes(user.token, { id: this.props.res.id })));
-        break;
-      case "not":
-        await this.vote(user.token, () => apiClient.uvRes(user.token, { id: this.props.res.id }));
-        break;
-    }
-  }
-
-  async onDV() {
-    const user = this.props.user.data;
-    if (user === null) {
-      return;
-    }
-
-    switch (this.props.res.voteFlag) {
-      case "dv":
-        await this.vote(user.token, () => apiClient.cvRes(user.token, { id: this.props.res.id }));
-        break;
-      case "uv":
-        await this.vote(user.token, () => apiClient.cvRes(user.token, { id: this.props.res.id })
-          .then(() => apiClient.dvRes(user.token, { id: this.props.res.id })));
-        break;
-      case "not":
-        await this.vote(user.token, () => apiClient.dvRes(user.token, { id: this.props.res.id }));
-        break;
-    }
-  }
-
-  async onDeleteClick() {
-    if (this.props.user.data === null) {
-      return;
-    }
-    const user = this.props.user.data;
-
-    try {
-      const reses = await resSetedCreate
-        .resSet(user.token, [await apiClient.delRes(user.token, { id: this.props.res.id })]);
-      if (this.props.update) {
-        this.props.update(reses[0]);
-      }
-    } catch {
-      this.setState({ snackMsg: "レス削除に失敗しました" });
-    }
-  }
-
   render(): JSX.Element {
     const smallIcon = {
       width: 18,
@@ -129,10 +62,8 @@ export const Res = myInject(["user"], observer(class extends React.Component<Unc
       padding: 8,
     };
 
-    const isSelf = this.props.user.data !== null && this.props.user.data.token.user === this.props.res.user;
-
     return this.props.user.data !== null
-      && !isSelf
+      && !this.props.res.self
       && !this.state.disableNG
       && this.props.user.data.storage.ng.some(x => ng.isNG(x, this.props.res))
       ? <div>あぼーん<a onClick={() => this.setState({ disableNG: true })}>[見る]</a></div>
@@ -141,49 +72,79 @@ export const Res = myInject(["user"], observer(class extends React.Component<Unc
           msg={this.state.snackMsg}
           onHide={() => this.setState({ snackMsg: null })} />
         <div className={style.vote}>
-          <IconButton
-            onClick={() => this.onUV()}
-            disabled={isSelf || this.props.user.data === null}>
-            <FontIcon
-              className="material-icons"
-              color={this.props.res.voteFlag === "uv" ? "orange" : undefined}>keyboard_arrow_up</FontIcon>
-          </IconButton>
-          <IconButton
-            onClick={() => this.onDV()}
-            disabled={isSelf || this.props.user.data === null}>
-            <FontIcon
-              className="material-icons"
-              color={this.props.res.voteFlag === "dv" ? "orange" : undefined}>keyboard_arrow_down</FontIcon>
-          </IconButton>
+          <Mutation<voteResResult, voteResVariables>
+            mutation={voteRes}
+            variables={{
+              res: this.props.res.id,
+              type: this.props.res.voteFlag === "uv" ? VoteType.cv : VoteType.uv
+            }}
+            onCompleted={data => {
+              if (this.props.update) {
+                this.props.update(data.voteRes);
+              }
+            }}>{(submit, { error }) => {
+              return (<>
+                {error && <Snack msg="投票に失敗しました" />}
+                <IconButton
+                  onClick={() => submit()}
+                  disabled={this.props.res.self || this.props.user.data === null}>
+                  <FontIcon
+                    className="material-icons"
+                    color={this.props.res.voteFlag === "uv" ? "orange" : undefined}>keyboard_arrow_up</FontIcon>
+                </IconButton>
+              </>);
+            }}</Mutation>
+          <Mutation<voteResResult, voteResVariables>
+            mutation={voteRes}
+            variables={{
+              res: this.props.res.id,
+              type: this.props.res.voteFlag === "dv" ? VoteType.cv : VoteType.dv
+            }}
+            onCompleted={data => {
+              if (this.props.update) {
+                this.props.update(data.voteRes);
+              }
+            }}>{(submit, { error }) => {
+              return (<>
+                {error && <Snack msg="投票に失敗しました" />}
+                <IconButton
+                  onClick={() => submit()}
+                  disabled={this.props.res.self || this.props.user.data === null}>
+                  <FontIcon
+                    className="material-icons"
+                    color={this.props.res.voteFlag === "dv" ? "orange" : undefined}>keyboard_arrow_down</FontIcon>
+                </IconButton>
+              </>);
+            }}</Mutation>
         </div>
         <div className={style.main}>
           <div className={classNames(style.header, {
-            [style.self]: isSelf,
-            [style.reply]: this.props.res.type === "normal" && this.props.res.isReply && !isSelf,
+            [style.self]: this.props.res.self,
+            [style.reply]: this.props.res.__typename === "ResNormal" && this.props.res.isReply && !this.props.res.self,
           })}>
             <a onClick={() => this.setState({ isReply: !this.state.isReply })}>
               #
               </a>
             &nbsp;
-              {this.props.res.type === "normal" && this.props.res.name !== null
+              {this.props.res.__typename === "ResNormal" && this.props.res.name !== null
               ? <span>{this.props.res.name}</span>
               : null}
-            {this.props.res.type === "normal" && this.props.res.name === null && this.props.res.profile === null
+            {this.props.res.__typename === "ResNormal" && this.props.res.name === null && this.props.res.profile === null
               ? <span>名無しさん</span>
               : null}
-            {this.props.res.type === "history"
+            {this.props.res.__typename === "ResHistory"
               ? <span>トピックデータ</span>
               : null}
-            {this.props.res.type === "topic"
+            {this.props.res.__typename === "ResTopic"
               ? <span>トピ主</span>
               : null}
-            {this.props.res.type === "fork"
+            {this.props.res.__typename === "ResFork"
               ? <span>派生トピック</span>
               : null}
-            {this.props.res.type === "delete"
+            {this.props.res.__typename === "ResDelete"
               ? <span>削除</span>
               : null}
-            {this.props.res.type === "normal" && this.props.res.profile !== null
+            {this.props.res.__typename === "ResNormal" && this.props.res.profile !== null
               ? <Link to={{
                 pathname: `/profile/${this.props.res.profile.id}`,
                 state: {
@@ -217,8 +178,20 @@ export const Res = myInject(["user"], observer(class extends React.Component<Unc
                 </IconButton>}
                 anchorOrigin={{ horizontal: "left", vertical: "top" }}
                 targetOrigin={{ horizontal: "left", vertical: "top" }}>
-                {isSelf && this.props.res.type === "normal"
-                  ? <MenuItem primaryText="削除" onClick={() => this.onDeleteClick()} />
+                {this.props.res.self && this.props.res.__typename === "ResNormal"
+                  ? <Mutation<delResResult, delResVariables>
+                    mutation={delRes}
+                    variables={{ res: this.props.res.id }}
+                    onCompleted={data => {
+                      if (this.props.update) {
+                        this.props.update(data.delRes);
+                      }
+                    }}>{(submit, { error }) => {
+                      return (<>
+                        {error && <Snack msg={"削除に失敗しました"} />}
+                        <MenuItem primaryText="削除" onClick={() => submit()} />
+                      </>);
+                    }}</Mutation>
                   : null}
                 <MenuItem primaryText="NG HASH" onClick={() => {
                   const user = this.props.user.data;
@@ -228,7 +201,7 @@ export const Res = myInject(["user"], observer(class extends React.Component<Unc
                       ng: user.storage.ng.insert(0, {
                         id: uuid.v4(),
                         name: `HASH:${this.props.res.hash}`,
-                        topic: this.props.res.topic,
+                        topic: this.props.res.topic.id,
                         date: new Date(),
                         expirationDate: null,
                         chain: 1,
@@ -242,10 +215,10 @@ export const Res = myInject(["user"], observer(class extends React.Component<Unc
                     });
                   }
                 }} />
-                {this.props.res.type === "normal" && this.props.res.profile !== null
+                {this.props.res.__typename === "ResNormal" && this.props.res.profile !== null
                   ? <MenuItem primaryText="NG Profile" onClick={() => {
                     const user = this.props.user.data;
-                    if (user !== null && this.props.res.type === "normal" && this.props.res.profile !== null) {
+                    if (user !== null && this.props.res.__typename === "ResNormal" && this.props.res.profile !== null) {
                       this.props.user.setStorage({
                         ...user.storage,
                         ng: user.storage.ng.insert(0, {
@@ -271,7 +244,7 @@ export const Res = myInject(["user"], observer(class extends React.Component<Unc
           </div>
           <div>
             <span>
-              {this.props.res.type === "normal" && this.props.res.reply !== null
+              {this.props.res.__typename === "ResNormal" && this.props.res.reply !== null
                 ? <IconButton
                   containerElement={<Link to={{
                     pathname: `/res/${this.props.res.reply}`,
@@ -297,21 +270,21 @@ export const Res = myInject(["user"], observer(class extends React.Component<Unc
                 </span>
                 : null}
             </span>
-            {this.props.res.type === "normal" ?
+            {this.props.res.__typename === "ResNormal" ?
               <Md text={this.props.res.text} />
-              : this.props.res.type === "history" ?
+              : this.props.res.__typename === "ResHistory" ?
                 <Md text={this.props.res.history.text} />
-                : this.props.res.type === "topic" && this.props.res.topicObject.type === "one" ?
-                  <Md text={this.props.res.topicObject.text} />
+                : this.props.res.__typename === "ResTopic" && this.props.res.topic.__typename === "TopicOne" ?
+                  <Md text={this.props.res.topic.text} />
                   : null}
-            {this.props.res.type === "topic" && this.props.res.topicObject.type === "fork"
+            {this.props.res.__typename === "ResTopic" && this.props.res.topic.__typename === "TopicFork"
               ? <div>
                 <p>
                   派生トピックが建ちました。
                     </p>
               </div>
               : null}
-            {this.props.res.type === "fork"
+            {this.props.res.__typename === "ResFork"
               ? <div>
                 <p>
                   派生トピック:<Link to={`/topic/${this.props.res.fork.id}`}>{this.props.res.fork.title}</Link>
@@ -319,7 +292,7 @@ export const Res = myInject(["user"], observer(class extends React.Component<Unc
               </div>
               : null}
 
-            {this.props.res.type === "delete"
+            {this.props.res.__typename === "ResDelete"
               ? <div>
                 <p>
                   {this.props.res.flag === "self"
@@ -332,7 +305,7 @@ export const Res = myInject(["user"], observer(class extends React.Component<Unc
           {this.state.isReply && this.props.user.data !== null
             ? <Paper>
               <ResWrite
-                topic={this.props.res.topic}
+                topic={this.props.res.topic.id}
                 reply={this.props.res.id}
                 userData={this.props.user.data}
                 changeStorage={x => this.props.user.setStorage(x)} />
