7d0730d92a215108a86fb76924f388b83a94e22c
kgtkr <kgtkr.jp@gmail.com>
2018-10-06 14:38:37 +0900

    とりあえずアカウントセッティング

--- src/pages/settings/account-setting.tsx
+++ src/pages/settings/account-setting.tsx
@@ -12,6 +12,12 @@ import {
 } from "react-router-dom";
 import { Errors, Snack, UserSwitch } from "../../components";
 import { myInject, UserStore } from "../../stores";
+import { Query, Mutation } from "react-apollo";
+import { findUser, updateUser } from "../../gql/user.gql";
+import { findUser as findUserResult } from "../../gql/_gql/findUser";
+import { updateUser as updateUserResult, updateUserVariables } from "../../gql/_gql/updateUser";
+import { createTokenMaster as createTokenMasterResult, createTokenMasterVariables } from "../../gql/_gql/createTokenMaster";
+import { createTokenMaster } from "../../gql/token.gql";
 
 interface AccountSettingPageProps extends RouteComponentProps<{}> {
   user: UserStore;
@@ -33,40 +39,6 @@ export const AccountSettingPage =
           oldPass: "",
           sn: "",
         };
-
-        (async () => {
-          if (this.props.user.data !== null) {
-            try {
-              const sn = await apiClient
-                .findUserSN({ id: this.props.user.data.token.user });
-              this.setState({ sn });
-            } catch {
-              this.setState({ snackMsg: "ユーザー情報取得に失敗しました。" });
-            }
-          }
-        })();
-      }
-
-      async onSubmit() {
-        if (this.props.user.data === null) {
-          return;
-        }
-        const user = this.props.user.data;
-        try {
-          await apiClient.updateUser({ id: user.token.user, pass: this.state.oldPass }, {
-            pass: this.state.newPass,
-            sn: this.state.sn,
-          });
-          const token = await apiClient.createTokenMaster({ id: user.token.user, pass: this.state.newPass });
-          this.props.user.updateToken(token);
-          this.setState({ errors: [] });
-        } catch (e) {
-          if (e instanceof AtError) {
-            this.setState({ errors: e.errors.map(e => e.message) });
-          } else {
-            this.setState({ errors: ["エラーが発生しました"] });
-          }
-        }
       }
 
       render() {
@@ -74,22 +46,46 @@ export const AccountSettingPage =
           <Helmet>
             <title>アカウント設定</title>
           </Helmet>
-          <Snack
-            msg={this.state.snackMsg}
-            onHide={() => this.setState({ snackMsg: null })} />
-          <form>
-            <Errors errors={this.state.errors} />
-            <TextField floatingLabelText="ID" value={this.state.sn} onChange={(_e, v) => this.setState({ sn: v })} />
-            <TextField
-              floatingLabelText="新しいパスワード"
-              value={this.state.newPass}
-              onChange={(_e, v) => this.setState({ newPass: v })} />
-            <TextField
-              floatingLabelText="現在のパスワード"
-              value={this.state.oldPass}
-              onChange={(_e, v) => this.setState({ oldPass: v })} />
-            <RaisedButton onClick={() => this.onSubmit()} label="OK" />
-          </form>
-        </Paper>} />;
+          <UserSwitch userData={this.props.user.data} render={() =>
+            <Query<findUserResult>
+              query={findUser}
+              onCompleted={user => {
+                if ("user" in user) {
+                  this.setState({ sn: user.user.sn });
+                }
+              }}>{
+                ({ error, data }) => {
+                  if (error) {
+                    return <Snack msg="ユーザー情報取得に失敗しました" />;
+                  }
+                  if (data === undefined) {
+                    return "loading";
+                  }
+                  return <Mutation<createTokenMasterResult, createTokenMasterVariables> mutation={createTokenMaster} onCompleted={x => {
+                    this.props.user.updateToken(x.createTokenMaster);
+                  }}>{submit => {
+                    return <Mutation<updateUserResult, updateUserVariables> mutation={updateUser} onCompleted={() => {
+                      submit({ variables: { auth: { id: data.user.id, pass: this.state.newPass } } });
+                    }} variables={{ auth: { id: data.user.id, pass: this.state.oldPass }, sn: this.state.sn, pass: this.state.newPass }}>{submit => {
+                      return <form>
+                        <TextField floatingLabelText="ID" value={this.state.sn} onChange={(_e, v) => this.setState({ sn: v })} />
+                        <TextField
+                          floatingLabelText="新しいパスワード"
+                          value={this.state.newPass}
+                          onChange={(_e, v) => this.setState({ newPass: v })} />
+                        <TextField
+                          floatingLabelText="現在のパスワード"
+                          value={this.state.oldPass}
+                          onChange={(_e, v) => this.setState({ oldPass: v })} />
+                        <RaisedButton onClick={() => submit()} label="OK" />
+                      </form>;
+                    }}</Mutation>;
+                  }}</Mutation>;
+                }
+              }</Query>
+          } />
+        </Paper >} />;
       }
     })));
+
+  //TODO:エラー処理
