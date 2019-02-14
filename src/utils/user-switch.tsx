926bdf4a49f818b61eea5d5b5bac4d4d3ace230a
kgtkr <kgtkr.jp@gmail.com>
2019-02-14 13:00:57 +0900

    UserSwitchProps

--- src/utils/user-switch.tsx
+++ src/utils/user-switch.tsx
@@ -3,9 +3,14 @@ import { UserData } from "src/models";
 import { Omit } from "type-zoo";
 import { UserContext } from "./user";
 
-export function userSwitch<P extends { userData: UserData, updateUserData: (value: UserData | null) => void }>(Children: React.ComponentType<P>)
-    : React.ComponentType<Omit<P, "userData" | "updateUserData">> {
-    return (props: Omit<P, "userData" | "updateUserData">) => {
+export interface UserSwitchProps {
+    userData: UserData,
+    updateUserData: (value: UserData | null) => void
+}
+
+export function userSwitch<P extends UserSwitchProps>(Children: React.ComponentType<P>)
+    : React.ComponentType<Omit<P, keyof UserSwitchProps>> {
+    return (props: Omit<P, keyof UserSwitchProps>) => {
         return <UserContext.Consumer>
             {val => val.value !== null
                 ? <Children {...props as any} userData={val.value} updateUserData={val.update} />
