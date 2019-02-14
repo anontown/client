09e44f340f9c3ed3c895a0e5265b40c84732daf9
kgtkr <kgtkr.jp@gmail.com>
2019-02-14 12:28:58 +0900

    User

---
+++ src/utils/user.tsx
@@ -0,0 +1,68 @@
+import * as React from "react";
+import { UserData } from "src/models";
+import * as rx from "rxjs";
+import * as op from "rxjs/operators";
+import { useEffectSkipN } from "./use-effect-skip-n";
+
+export interface UserContextType {
+  value: UserData | null,
+  update: (value: UserData | null) => void
+}
+
+export const UserContext = React.createContext<UserContextType>({
+  value: null,
+  update: () => { }
+});
+
+export interface UserProps {
+  children: React.ReactNode,
+  initUserData: UserData | null,
+}
+
+export const User = (props: UserProps) => {
+  const [userData, setUserData] = React.useState(props.initUserData);
+  const subjectRef = React.useRef(new rx.Subject<UserData | null>());
+  useEffectSkipN(() => {
+    subjectRef.current.next(userData);
+  }, [userData]);
+  React.useEffect(() => {
+    const subs = subjectRef
+      .current
+      .pipe(op.debounceTime(5000))
+      .subscribe(data => {
+        if (data !== null) {
+          storageAPI
+            .save(data.token, data.storage);
+        }
+      });
+
+    return () => {
+      subs.unsubscribe();
+    };
+  }, []);
+
+  useEffectSkipN(() => {
+    if (userData !== null) {
+      localStorage.setItem("token", JSON.stringify({
+        id: userData.token.id,
+        key: userData.token.key,
+      }));
+    } else {
+      localStorage.removeItem("token");
+    }
+  }, [userData !== null ? userData.token.id : null]);
+
+  useEffectSkipN(() => {
+    location.reload();
+  }, [userData !== null ? userData.token.user : null]);
+
+  return (
+    <UserContext.Provider
+      value={{
+        value: userData,
+        update: x => setUserData(x)
+      }}>
+      {props.children}
+    </UserContext.Provider>
+  );
+};
