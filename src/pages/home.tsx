1f1b33032eaa7db699aee995f0448045a4f5592a
kgtkr <kgtkr.jp@gmail.com>
2019-02-25 15:19:06 +0900

    HomePage

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,57 +3,52 @@ import {
   Tab,
   Tabs,
 } from "material-ui";
-import { observer } from "mobx-react";
 import * as React from "react";
 import { Helmet } from "react-helmet";
 import {
   Link,
-  RouteComponentProps,
-  withRouter,
 } from "react-router-dom";
 import { Page } from "../components";
 import {
   TagFavo,
   TopicFavo,
 } from "../components";
-import { myInject, UserStore } from "../stores";
+import { useUserContext } from "src/utils";
 
-interface HomePageProps extends RouteComponentProps<{}> {
-  user: UserStore;
-}
+interface HomePageProps { }
 
-export const HomePage = withRouter(myInject(["user"], observer
-  ((props: HomePageProps) => {
-    return (
-      <Page>
-        <Helmet>
-          <title>Anontown</title>
-        </Helmet>
-        {props.user.data !== null
-          ? <Tabs>
-            <Tab label="トピック">
-              <TopicFavo detail={true} userData={props.user.data} />
-            </Tab>
-            <Tab label="タグ">
-              <TagFavo userData={props.user.data} />
-            </Tab>
-          </Tabs>
-          : <Paper>
-            <h1>匿名掲示板Anontownへようこそ</h1>
-            <ul>
-              <li>
-                <Link to="/topic/search">トピック一覧</Link>
-              </li>
-              <li>
-                <a
-                  href="https://document.anontown.com/"
-                  target="_blank"
-                >
-                  説明書
+export const HomePage = (_props: HomePageProps) => {
+  const userContext = useUserContext();
+  return (
+    <Page>
+      <Helmet>
+        <title>Anontown</title>
+      </Helmet>
+      {userContext.value !== null
+        ? <Tabs>
+          <Tab label="トピック">
+            <TopicFavo detail={true} userData={userContext.value} />
+          </Tab>
+          <Tab label="タグ">
+            <TagFavo userData={userContext.value} />
+          </Tab>
+        </Tabs>
+        : <Paper>
+          <h1>匿名掲示板Anontownへようこそ</h1>
+          <ul>
+            <li>
+              <Link to="/topic/search">トピック一覧</Link>
+            </li>
+            <li>
+              <a
+                href="https://document.anontown.com/"
+                target="_blank"
+              >
+                説明書
                 </a>
-              </li>
-            </ul>
-          </Paper>}
-      </Page>
-    );
-  })));
+            </li>
+          </ul>
+        </Paper>}
+    </Page>
+  );
+};
