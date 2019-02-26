747292276318667071c0cfb68e318fe178ee6447
kgtkr <kgtkr.jp@gmail.com>
2019-02-26 19:37:00 +0900

    Array

--- src/utils/list.ts
+++ src/utils/list.ts
@@ -1,7 +1,5 @@
-import * as Im from "immutable";
-
-export function update<T extends { id: string }>(list: Im.List<T>, item: T) {
-  const index = list.findIndex( x => x.id === item.id);
+export function update<T extends { id: string }>(list: T[], item: T) {
+  const index = list.findIndex(x => x.id === item.id);
   if (index !== -1) {
     return list.set(index, item);
   } else {
