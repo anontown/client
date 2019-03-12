961742dde0bac562ea302e51765a02663ff3777c
kgtkr <kgtkr.jp@gmail.com>
2019-03-12 21:14:51 +0900

    ResPage

--- src/pages/res.tsx
+++ src/pages/res.tsx
@@ -1,5 +1,4 @@
 import { Paper } from "material-ui";
-import { observer } from "mobx-react";
 import * as React from "react";
 import { Helmet } from "react-helmet";
 import {
@@ -7,50 +6,44 @@ import {
   withRouter,
 } from "react-router-dom";
 import { Page, Res, Snack } from "../components";
-import { myInject, UserStore } from "../stores";
 import {
   withModal,
 } from "../utils";
-import { findReses } from "../gql/res.gql";
-import { findReses as findResesResult, findResesVariables } from "../gql/_gql/findReses";
-import { Query } from "react-apollo";
+import * as G from "../../generated/graphql";
 
 interface ResBaseProps extends RouteComponentProps<{ id: string }> {
-  user: UserStore;
   zDepth?: number;
 }
 
 interface ResBaseState {
 }
 
-const ResBase = withRouter(myInject(["user"],
-  observer(class extends React.Component<ResBaseProps, ResBaseState> {
-    constructor(props: ResBaseProps) {
-      super(props);
-    }
+const ResBase = withRouter((class extends React.Component<ResBaseProps, ResBaseState> {
+  constructor(props: ResBaseProps) {
+    super(props);
+  }
 
-    render() {
-      return <div>
-        <Helmet>
-          <title>レス</title>
-        </Helmet>
-        <Query<findResesResult, findResesVariables>
-          query={findReses}
-          variables={{ query: { id: [this.props.match.params.id] } }}>
-          {({ loading, error, data }) => {
-            if (loading) return "Loading...";
-            if (error || !data || data.reses.length === 0) return (<Snack msg="レス取得に失敗しました" />);
+  render() {
+    return <div>
+      <Helmet>
+        <title>レス</title>
+      </Helmet>
+      <G.FindReses.Component
+        variables={{ query: { id: [this.props.match.params.id] } }}>
+        {({ loading, error, data }) => {
+          if (loading) return "Loading...";
+          if (error || !data || data.reses.length === 0) return (<Snack msg="レス取得に失敗しました" />);
 
-            return (
-              <Paper zDepth={this.props.zDepth}>
-                <Res res={data.reses[0]} />
-              </Paper>
-            );
-          }}
-        </Query>
-      </div>;
-    }
-  })));
+          return (
+            <Paper zDepth={this.props.zDepth}>
+              <Res res={data.reses[0]} />
+            </Paper>
+          );
+        }}
+      </G.FindReses.Component>
+    </div>;
+  }
+}));
 
 export function ResPage() {
   return <Page><ResBase /></Page>;
