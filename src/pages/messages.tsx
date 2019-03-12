4e44c4b17a691788dc9256b74bec4ca72ca3a1bf
kgtkr <kgtkr.jp@gmail.com>
2019-03-12 19:44:43 +0900

    MessagesPage

--- src/pages/messages.tsx
+++ src/pages/messages.tsx
@@ -1,10 +1,7 @@
-import * as api from "@anontown/api-types";
-import * as Im from "immutable";
 import {
   Paper,
   RaisedButton,
 } from "material-ui";
-import { observer } from "mobx-react";
 import * as React from "react";
 import { Helmet } from "react-helmet";
 import { withRouter } from "react-router";
@@ -12,21 +9,22 @@ import { RouteComponentProps } from "react-router-dom";
 import {
   Md,
   Page,
-  Snack,
-  UserSwitch,
 } from "../components";
-import { myInject, UserStore } from "../stores";
-import { apiClient, dateFormat, userSwitch, UserSwitchProps } from "../utils";
-import { findMsgs } from "../gql/msg.gql";
-import { findMsgs as findMsgsResult, findMsgsVariables } from "../gql/_gql/findMsgs";
-import { useQuery } from "react-apollo-hooks";
+import { dateFormat, userSwitch, UserSwitchProps } from "../utils";
+import * as G from "../../generated/graphql";
 
 type MessagesPageProps = RouteComponentProps<{}> & UserSwitchProps;
 
 export const MessagesPage = userSwitch(withRouter((props: MessagesPageProps) => {
-  const limit = 50;
-  const msgs = useQuery<findMsgsResult, findMsgsVariables>(findMsgs, {
-
+  const msgs = G.FindMsgs.use({
+    variables: {
+      query: {
+        date: {
+          date: new Date().toISOString(),
+          type: G.DateType.Lte
+        }
+      }
+    }
   });
 
   return (
@@ -34,137 +32,75 @@ export const MessagesPage = userSwitch(withRouter((props: MessagesPageProps) => 
       <Helmet>
         <title>お知らせ</title>
       </Helmet>
-      <Snack
-        msg={this.state.snackMsg}
-        onHide={() => this.setState({ snackMsg: null })} />
-      <UserSwitch userData={this.props.user.data} render={() => <div>
+      <div>
         <div>
-          <RaisedButton label="最新" onClick={() => this.readNew()} />
+          <RaisedButton label="最新" onClick={async () => {
+            if (msgs.data === undefined) {
+              return;
+            }
+            const first = msgs.data.msgs.first();
+            if (first === undefined) {
+              await msgs.refetch();
+            } else {
+              msgs.fetchMore({
+                variables: {
+                  query: {
+                    date: {
+                      date: first.date,
+                      type: G.DateType.Gt
+                    }
+                  },
+                },
+                updateQuery: (prev, { fetchMoreResult }) => {
+                  if (!fetchMoreResult) return prev;
+                  return {
+                    ...prev,
+                    msgs: [...fetchMoreResult.msgs, ...prev.msgs]
+                  }
+                }
+              });
+            }
+          }} />
         </div>
         <div>
-          {this.state.msgs.map(m =>
-            <Paper key={m.id}>
-              <div>{dateFormat.format(m.date)}</div>
-              <Md text={m.text} />
-            </Paper>)}
+          {msgs.data !== undefined
+            ? msgs.data.msgs.map(m =>
+              <Paper key={m.id}>
+                <div>{dateFormat.format(m.date)}</div>
+                <Md text={m.text} />
+              </Paper>)
+            : null}
         </div>
         <div>
-          <RaisedButton label="前" onClick={() => this.readOld()} />
+          <RaisedButton label="前" onClick={async () => {
+            if (msgs.data === undefined) {
+              return;
+            }
+            const last = msgs.data.msgs.last();
+            if (last === undefined) {
+              await msgs.refetch();
+            } else {
+              msgs.fetchMore({
+                variables: {
+                  query: {
+                    date: {
+                      date: last.date,
+                      type: G.DateType.Lt
+                    }
+                  },
+                },
+                updateQuery: (prev, { fetchMoreResult }) => {
+                  if (!fetchMoreResult) return prev;
+                  return {
+                    ...prev,
+                    msgs: [...prev.msgs, ...fetchMoreResult.msgs]
+                  }
+                }
+              });
+            }
+          }} />
         </div>
-      </div>} />
+      </div>
     </Page>
   );
 }));
-
-interface MessagesPageState {
-  msgs: Im.List<api.Msg>;
-  snackMsg: null | string;
-}
-
-export const _MessagesPage = withRouter(myInject(["user"],
-  observer(class extends React.Component<MessagesPageProps, MessagesPageState> {
-    private limit = 50;
-
-    constructor(props: MessagesPageProps) {
-      super(props);
-      this.state = {
-        msgs: Im.List(),
-        snackMsg: null,
-      };
-
-      this.findNew();
-    }
-
-    render() {
-      return (
-        <Page>
-          <Helmet>
-            <title>お知らせ</title>
-          </Helmet>
-          <Snack
-            msg={this.state.snackMsg}
-            onHide={() => this.setState({ snackMsg: null })} />
-          <UserSwitch userData={this.props.user.data} render={() => <div>
-            <div>
-              <RaisedButton label="最新" onClick={() => this.readNew()} />
-            </div>
-            <div>
-              {this.state.msgs.map(m =>
-                <Paper key={m.id}>
-                  <div>{dateFormat.format(m.date)}</div>
-                  <Md text={m.text} />
-                </Paper>)}
-            </div>
-            <div>
-              <RaisedButton label="前" onClick={() => this.readOld()} />
-            </div>
-          </div>} />
-        </Page>
-      );
-    }
-
-    async findNew() {
-      if (this.props.user.data === null) {
-        return;
-      }
-
-      try {
-        const msgs = await apiClient.findMsg(this.props.user.data.token,
-          {
-            limit: this.limit,
-            date: new Date().toISOString(),
-            type: "lte",
-          });
-        this.setState({ msgs: Im.List(msgs) });
-      } catch {
-        this.setState({ snackMsg: "メッセージ取得に失敗" });
-      }
-    }
-
-    async readNew() {
-      if (this.props.user.data === null) {
-        return;
-      }
-
-      const first = this.state.msgs.first();
-      if (first === undefined) {
-        await this.findNew();
-      } else {
-        try {
-          const msgs = await apiClient.findMsg(this.props.user.data.token,
-            {
-              type: "gt",
-              date: first.date,
-              limit: this.limit,
-            });
-          this.setState({ msgs: Im.List(msgs).concat(this.state.msgs) });
-        } catch {
-          this.setState({ snackMsg: "メッセージ取得に失敗" });
-        }
-      }
-    }
-
-    async readOld() {
-      if (this.props.user.data === null) {
-        return;
-      }
-      const last = this.state.msgs.last();
-
-      if (last === undefined) {
-        await this.findNew();
-      } else {
-        try {
-          const msgs = await apiClient.findMsg(this.props.user.data.token,
-            {
-              type: "lt",
-              date: last.date,
-              limit: this.limit,
-            });
-
-          this.setState({ msgs: this.state.msgs.concat(msgs) });
-        } catch {
-          this.setState({ snackMsg: "メッセージ取得に失敗" });
-        }
-      }
-    }
-  })));
