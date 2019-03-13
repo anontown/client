bca3d6a98366ad60fa7f552e999c1ce6aac21d55
kgtkr <kgtkr.jp@gmail.com>
2019-03-13 16:40:22 +0900

    TopicEditPage

--- src/pages/topic-edit.tsx
+++ src/pages/topic-edit.tsx
@@ -1,6 +1,4 @@
-import * as api from "@anontown/api-types";
 import { Paper } from "material-ui";
-import { observer } from "mobx-react";
 import * as React from "react";
 import { Helmet } from "react-helmet";
 import {
@@ -9,66 +7,38 @@ import {
 } from "react-router-dom";
 import {
   Page,
-  Snack,
   TopicEditor,
-  UserSwitch,
 } from "../components";
-import { myInject, UserStore } from "../stores";
 import {
-  apiClient,
   withModal,
+  userSwitch,
+  UserSwitchProps,
 } from "../utils";
+import * as G from "../../generated/graphql";
 
-interface TopicEditBaseProps extends RouteComponentProps<{ id: string }> {
+type TopicEditBaseProps = RouteComponentProps<{ id: string }> & UserSwitchProps & {
   zDepth?: number;
-  user: UserStore;
-}
+};
 
-interface TopicEditBaseState {
-  topic: api.TopicNormal | null;
-  snackMsg: null | string;
-}
-
-const TopicEditBase = withRouter(myInject(["user"],
-  observer(class extends React.Component<TopicEditBaseProps, TopicEditBaseState> {
-    constructor(props: TopicEditBaseProps) {
-      super(props);
-      this.state = {
-        topic: null,
-        snackMsg: null,
-      };
-      (async () => {
-        try {
-          const topic = await apiClient.findTopicOne({
-            id: this.props.match.params.id,
-          });
-          if (topic.type === "normal") {
-            this.setState({ topic });
-          } else {
-            this.setState({ snackMsg: "通常トピックではありません。" });
-          }
-        } catch {
-          this.setState({ snackMsg: "トピック取得に失敗しました" });
-        }
-      })();
+const TopicEditBase = withRouter(userSwitch((props: TopicEditBaseProps) => {
+  const topics = G.FindTopics.use({
+    variables: {
+      query: {
+        id: [props.match.params.id]
+      }
     }
+  });
+  const topic = topics.data !== undefined ? topics.data.topics[0] : null;
 
-    render() {
-      return <UserSwitch userData={this.props.user.data} render={userData => <Paper zDepth={this.props.zDepth}>
-        <Helmet>
-          <title>トピック編集</title>
-        </Helmet>
-        <Snack
-          msg={this.state.snackMsg}
-          onHide={() => this.setState({ snackMsg: null })} />
-        {this.state.topic !== null && this.state.topic.type === "normal"
-          ? <TopicEditor topic={this.state.topic} onUpdate={topic => {
-            this.setState({ topic });
-          }} userData={userData} />
-          : null}
-      </Paper>} />;
-    }
-  })));
+  return <Paper zDepth={props.zDepth}>
+    <Helmet>
+      <title>トピック編集</title>
+    </Helmet>
+    {topic !== null && topic.__typename === "TopicNormal"
+      ? <TopicEditor topic={topic} userData={props.userData} />
+      : null}
+  </Paper>;
+}));
 
 export function TopicEditPage() {
   return <Page><TopicEditBase /></Page>;
