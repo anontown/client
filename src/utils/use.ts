35cff4db5d1942128c932bd66423cfcd5da7299c
kgtkr <kgtkr.jp@gmail.com>
2019-03-20 15:23:10 +0900

    useEffectCond

--- src/utils/use.ts
+++ src/utils/use.ts
@@ -11,3 +11,17 @@ export function useEffectSkipN(effect: React.EffectCallback, deps?: React.Depend
     }
   }, deps);
 }
+
+export function useEffectCond(effect: React.EffectCallback, cond: () => boolean, n = Infinity) {
+  const countRef = React.useRef(0);
+
+  React.useEffect(() => {
+    if (cond()) {
+      if (countRef.current < n) {
+        effect();
+      } else {
+        countRef.current++;
+      }
+    }
+  });
+}
