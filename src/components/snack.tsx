d4dd767316922af08dc331fb0c329c19994ca8e4
kgtkr <kgtkr.jp@gmail.com>
2019-04-01 19:35:30 +0900

    スナックバーのwarn修正

--- src/components/snack.tsx
+++ src/components/snack.tsx
@@ -2,12 +2,13 @@ import {
   Snackbar,
 } from "material-ui";
 import * as React from "react";
+import { nullUnwrapOr } from "@kgtkr/utils";
 
 export function Snack(props: { msg: string | null, onHide?: () => void }) {
   return (
     <Snackbar
       open={props.msg !== null}
-      message={props.msg}
+      message={nullUnwrapOr("")(props.msg)}
       autoHideDuration={5000}
       onRequestClose={() => {
         if (props.onHide) {
